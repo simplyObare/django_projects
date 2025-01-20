a6dc72680c6d899ee19f6b54f4500415a60035d5
simplyObare <josephatowiny@gmail.com>
2025-01-20 08:11:47 +0300

    django admin

--- basic_CRM/leads/admin.py
+++ basic_CRM/leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lead, Agent, User
 
 # Register your models here.
+admin.site.register(Lead)
+admin.site.register(Agent)
+admin.site.register(User)
