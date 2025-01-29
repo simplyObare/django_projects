047027f1a81d6d4ec5ee3322b95e2c44b11ef648
simplyObare <josephatowiny@gmail.com>
2025-01-29 15:29:36 +0300

    creating view

--- simple_blog/account/views.py
+++ simple_blog/account/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework import status
 
 # Create your views here.
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        try:
+            serializer = RegisterSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(e.args[0], status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        return Response()
