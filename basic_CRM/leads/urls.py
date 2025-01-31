3b7d1803c0fb75c982bc8c05c9cc8a22e4749b2c
simplyObare <josephatowiny@gmail.com>
2025-01-31 03:43:10 +0300

    Forms and Create View

--- basic_CRM/leads/urls.py
+++ basic_CRM/leads/urls.py
@@ -5,5 +5,6 @@ app_name = "leads"
 
 urlpatterns = [
     path("", views.lead_list, name="lead_list"),
-    path("<pk>/", views.lead_detail, name="lead_detail"),
+    path("<int:pk>/", views.lead_detail, name="lead_detail"),
+    path("create/", views.lead_create, name="lead_create"),
 ]
--- basic_CRM/leads/views.py
+++ basic_CRM/leads/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
 def lead_list(request):
@@ -14,3 +15,21 @@ def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {"lead": lead}
     return render(request, "lead_detail.html", context)
+
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            age = form.cleaned_data["age"]
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name, last_name=last_name, age=age, agent=agent
+            )
+            return redirect("/leads")
+
+    context = {"form": LeadForm()}
+    return render(request, "lead_create.html", context)
