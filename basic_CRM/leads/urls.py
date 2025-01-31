18c43d24c784582aa1000524160f20c7d0c77190
simplyObare <josephatowiny@gmail.com>
2025-01-31 06:20:25 +0300

    Lead Update View and Lead Delete View

--- basic_CRM/leads/urls.py
+++ basic_CRM/leads/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.lead_list, name="lead_list"),
     path("<int:pk>/", views.lead_detail, name="lead_detail"),
     path("create/", views.lead_create, name="lead_create"),
+    path("<int:pk>/update/", views.lead_update, name="lead_update"),
+    path("<int:pk>/delete/", views.lead_delete, name="lead_delete"),
 ]
--- basic_CRM/leads/views.py
+++ basic_CRM/leads/views.py
@@ -29,19 +29,23 @@ def lead_create(request):
     return render(request, "lead_create.html", context)
 
 
-# def lead_create(request):
-#     form = LeadModelForm()
-#     if request.method == "POST":
-#         form = LeadModelForm(request.POST)
-#         if form.is_valid():
-#             first_name = form.cleaned_data["first_name"]
-#             last_name = form.cleaned_data["last_name"]
-#             age = form.cleaned_data["age"]
-#             agent = Agent.objects.first()
-#             Lead.objects.create(
-#                 first_name=first_name, last_name=last_name, age=age, agent=agent
-#             )
-#             return redirect("/leads")
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
 
-#     context = {"form": LeadModelForm()}
-#     return render(request, "lead_create.html", context)
+    context = {
+        "form": form,
+        "lead": lead,
+    }
+    return render(request, "lead_update.html", context)
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
